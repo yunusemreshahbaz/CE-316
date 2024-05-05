5788a1bfacb7f356a00499cb952ef2576c0c230a
Yunus <yunusdxn@hotmail.com>
2024-05-05 21:30:54 +0300

    a specific compiler class for C

---
+++ src/main/java/com/teamnine/ce316iae/CompilerOfC.java
@@ -0,0 +1,16 @@
+package com.teamnine.ce316iae;
+
+import java.io.IOException;
+
+public class CompilerOfC implements Compiler {
+    @Override
+    public int compile(String compilerPath, String[] arguments) throws IOException, InterruptedException {
+        String[] commandarray = new String[arguments.length + 1];
+        System.arraycopy(arguments, 0, commandarray, 1, arguments.length);
+        commandarray[0] = compilerPath;
+        System.out.println(commandarray[0] + commandarray[1]);
+        Process compilationProcess = Runtime.getRuntime().exec(commandarray);
+        compilationProcess.waitFor();
+        return compilationProcess.exitValue();
+    }    
+}
