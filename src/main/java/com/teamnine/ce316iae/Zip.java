0918c5bc81efd82616c2a60e6e35affc8c2a9bb7
Yunus <yunusdxn@hotmail.com>
2024-05-05 21:32:40 +0300

    a class for zip extraction

--- src/main/java/com/teamnine/ce316iae/Zip.java
+++ src/main/java/com/teamnine/ce316iae/Zip.java
@@ -1,7 +1,13 @@
 package com.teamnine.ce316iae;
 
-// import java.io.*;
-// import java.util.zip.*;
+//imports
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Zip{
 
@@ -18,6 +24,49 @@ public class Zip{
 
     // Methods
     public static void extractZipFiles(String zipDirectory) {
-    
+        File zip = new File(zipDirectory);
+        File[] zFiles = zip.listFiles((dir, name) -> name.toLowerCase().endsWith(".zip"));
+        byte[] size = new byte[1024];
+
+        if(zFiles != null) {
+            for(File zFile : zFiles) {
+                try{
+                    ZipInputStream zInputStream = new ZipInputStream(new FileInputStream(zFile));
+                    ZipEntry zEntry = zInputStream.getNextEntry();
+
+                    while(zEntry != null){
+                        File fileOfOutput = new File(zip, zEntry.getName());
+                        
+                        if(zEntry.isDirectory()){
+                            if(!fileOfOutput.exists()){
+                                fileOfOutput.mkdirs();
+                            }
+                        } else{
+                            File parentDirectory = fileOfOutput.getParentFile();
+
+                            if(!parentDirectory.exists()){
+                                parentDirectory.mkdirs();
+                            }
+
+                            FileOutputStream fileOutputStream = new FileOutputStream(fileOfOutput);
+                            int zipLength;
+
+                            while((zipLength = zInputStream.read(size)) > 0){
+                                fileOutputStream.write(size, 0, zipLength);
+                            }
+                            fileOutputStream.close();
+                        }
+                        zEntry = zInputStream.getNextEntry();
+                    }
+                    zInputStream.closeEntry();
+                    zInputStream.close();
+                }
+                catch(IOException e){
+                    e.getMessage();
+                }
+            }
+        } else{
+            System.out.println("There is currently no Zip");
+        }
     }
 }
