a993674b39f918b22151e19e1f5b0a7aa00acf3a
Yunus <yunusdxn@hotmail.com>
2024-05-05 22:43:36 +0300

    Refactor compile method in CompilerOfC to use ProcessBuilder

--- src/main/java/com/teamnine/ce316iae/Compilers/CompilerOfC.java
+++ src/main/java/com/teamnine/ce316iae/Compilers/CompilerOfC.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 
 public class CompilerOfC implements Compiler {
     @Override
-    public int compile(String compilerPath, String[] arguments) throws IOException, InterruptedException {
-        String[] commandarray = new String[arguments.length + 1];
-        System.arraycopy(arguments, 0, commandarray, 1, arguments.length);
-        commandarray[0] = compilerPath;
-        System.out.println(commandarray[0] + commandarray[1]);
-        Process compilationProcess = Runtime.getRuntime().exec(commandarray);
+    public int compile(String compilerPath, String arguments) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(compilerPath, arguments);
+        Process compilationProcess = processBuilder.start();
         compilationProcess.waitFor();
         return compilationProcess.exitValue();
     }    
